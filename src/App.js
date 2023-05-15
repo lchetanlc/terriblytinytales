611d5aa41d267c646fffa1e84812f6ed7ddc7b5c
lchetanlc <lchetan2002@gmail.com>
2023-05-16 02:40:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+import './App.css'
+const App = () => {
+  const [showGraph, setShowGraph] = useState(false);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  const handleButtonClick = () => {
+    setShowGraph(true);
+  };
+  const SubmitPage = ({ onButtonClick }) => {
+    return (
+      <div className="container">
+       <div className="button_container">
+        <p className="description"> React Word Frequency Analyser.
+        
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button className="btn" onClick={onButtonClick}><span>Submit !</span></button>
+      </div>
+      </div>
+    );
+  };
+  const GraphPage = () => {
+    const [words, setWords] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+      fetchWordData();
+    }, []);
+
+    const fetchWordData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch('https://www.terriblytinytales.com/test.txt');
+        const text = await response.text();
+
+        const wordList = text.split(/\s+/);
+        const wordFrequency = {};
+
+        wordList.forEach((word) => {
+          wordFrequency[word] = (wordFrequency[word] || 0) + 1;
+        });
+
+        const sortedWords = Object.entries(wordFrequency).sort((a, b) => b[1] - a[1]);
+
+        setWords(sortedWords.slice(0, 20));
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching word data:', error);
+        setLoading(false);
+      }
+    };
+
+    const exportCSV = () => {
+      const csvContent = [
+        ['Word', 'Frequency'],
+        ...words.map(([word, frequency]) => [word, frequency]),
+      ]
+        .map((row) => row.join(','))
+        .join('\n');
+
+      const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+      const link = document.createElement('a');
+      const url = URL.createObjectURL(blob);
+
+      link.href = url;
+      link.setAttribute('download', 'word_frequency.csv');
+      link.style.visibility = 'hidden';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    };
+
+    const chartData = {
+      labels: words.map(([word]) => word),
+      datasets: [
+        {
+          label: 'Word Frequency',
+          data: words.map(([, frequency]) => frequency),
+          backgroundColor: 'rgba(68, 126, 203, 1)',
+        },
+      ],
+    };
+
+    const chartOptions = {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true,
+          stepSize: 1,
+        },
+      },
+    };
+
+    return (
+      <div className="container">
+
+        <div className="chart-container">
+          <Bar data={chartData} options={chartOptions} />
+        </div>
+        <div className="button-group">
+          <div> <a className="btn-slide" onClick={exportCSV}>
+            <span className="circle">
+              <i className="fa fa-save"></i>
+            </span>
+            <span className="title" >Export Csv</span>
+            <span className="title title-hover">Download Now</span>
+          </a> </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      {!showGraph ? (
+        <SubmitPage onButtonClick={handleButtonClick} />
+      ) : (
+        <GraphPage />
+      )}
     </div>
   );
-}
+};
 
 export default App;
